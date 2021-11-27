f22884df4625d11e53aee1e06bcaedf9bd1e2515
Ariel Montenegro <ariel.montenegro@biometric.io>
2021-11-27 18:05:02 -0300

    first commit

---
+++ app/locations/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Location
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'latitude','longitude' ,'date_time']
+    search_fields = ('user',)
+
+    def save_model(self, request, obj, form, change):
+            obj.user = request.user
+            super().save_model(request, obj, form, change)
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("user", )
+        form = super(LocationAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+admin.site.register(Location, LocationAdmin)
---
+++ app/locations/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Location(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    date_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'answer',)
