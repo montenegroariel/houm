a7bbcaf8b714047c4939b3a5e01c3fa2887e8012
Ariel Montenegro <ariel.montenegro@biometric.io>
2021-11-30 09:49:59 -0300

    postman files is added

--- app/locations/views.py
+++ app/locations/views.py
@@ -14,6 +14,18 @@ from math import sin, cos, sqrt, atan2, radians
 
 
 class LocationViewSet(viewsets.ModelViewSet):
+    """The endpoint receives latitude and 
+    longitude, in case of not changing 
+    position the final time is updated. 
+    When changing position a new record is created.
+
+    Args:
+        (float): latitude
+        (float): longitude
+
+    Returns:
+        [json]: Serialized location object
+    """
     permission_classes = (IsAuthenticated,)
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
@@ -21,37 +33,67 @@ class LocationViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
+            # Check posiiton
             location = Location.objects.filter(user=request.user, latitude=request.data['latitude'], longitude=request.data['longitude']).last()
+
             if location:
+                # Update final time
                 location.end = datetime.now()
                 location.save()
                 return Response(serializer.data, status=status.HTTP_304_NOT_MODIFIED)            
             else:
+                # Create new position
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TimeViewSet(generics.ListAPIView):
+    """The duration of time is obtained 
+    in a coordinate. The difference 
+    between end point and start point
+
+    Args:
+        user: Id object user in url 
+        /time/<int:user>.
+
+    Returns:
+        [json]: Serialized list of locations 
+        over time in each coordinate
+    """
     permission_classes = (IsAuthenticated,)
     serializer_class = TimeSerializer
 
     def list(self, request, *args, **kwargs):
         time_list=[]
-        user_id = kwargs["user_id"]
+        user_id = kwargs["user_id"] # User id in url
         locations = Location.objects.filter(user_id=user_id)
+
         for location in locations:
             time_list.append({
                 "user" : location.user,
                 "latitude" : location.latitude,
                 "longitude" : location.longitude,
-                "time": str(location.end - location.begin),
+                "time": str(location.end - location.begin), # time in location
             })
         time = TimeSerializer(instance=time_list, many=True)
         return Response(time.data)
     
 
 class VelocityViewSet(generics.ListAPIView):
+    """The speed and distance are obtained 
+    with the coordinates and the time between 
+    each point.
+
+    Args:
+        user: Id object user in url 
+        velocity: Float number for filter velocity.
+        /time/<int:user>/<float:velocity>.
+
+    Returns:
+        [json]: Serialized list of locations 
+        over time, velocity and distance.
+    """
     permissions_classes = (IsAuthenticated,)
     serializers_class = VelocitySerializer
 
@@ -86,8 +128,9 @@ class VelocityViewSet(generics.ListAPIView):
                                         velocity_list[-1]["longitude_end"])
 
             velocity = self.get_velocity(distance, velocity_list[-1]["time_start"], velocity_list[-1]["time_end"])
-        result = filter(lambda x: x["velocity"] > kwargs["velocity"], velocity_list)
-        total = VelocitySerializer(instance=result, many=True)
+        
+        velocity_list = filter(lambda x: x["velocity"] > kwargs["velocity"], velocity_list)
+        total = VelocitySerializer(instance=velocity_list, many=True)
         return Response(total.data)
 
 
